3121f903d953429572edbece86ec02155a857d5c
Nitya-Prakash <nityaprakashsahoo677@gmail.com>
2024-08-17 19:58:55 +0530

    Making it reponsive

--- script.js
+++ script.js
@@ -1,145 +1,232 @@
-function canAnimation() {
-    let tl = gsap.timeline({
-        scrollTrigger: {
-            trigger: ".page2",
-            start: "0% 90%",
-            end: "70% 50%",
-            scrub: true,
-        }
-    })
 
-    tl.to("#can", {
-        top: "115%",
-        left: "15%"
-    }, "lime")
+let mm = gsap.matchMedia();
 
-    tl.to("#lime", {
-        width: "12%",
-        top: "160%",
-        left: "25%",
-        zIndex: 1
-    }, "lime")
-
-    tl.to("#leaf", {
-        top: "110%",
-        left: "75%",
-        zIndex: 1
-    }, "lime")
-
-    tl.to("#ice1", {
-        top: "120%",
-        left: "7%",
-        zIndex: 1
-    }, "lime")
-
-
-    let tl2 = gsap.timeline({
-        scrollTrigger: {
-            trigger: ".page3",
-            start: "0% 95%",
-            end: "25% 30%",
-            scrub: true,
-        }
-    })
-
-    tl2.to("#can", {
-        top: "212%",
-        left: "67.5%",
-        width: "15%",
-        display: "none"
-    })
-
-
-    gsap.to(".cardImage2", {
-        display: "block",
-        scrollTrigger: {
-            trigger: ".page3",
-            start: "0% 7%",
-            scrub: true,
-        }
-    })
-}
-
-let card = document.querySelectorAll(".card");
-
-function cardAnimation() {
-    card.forEach(function (cards) {
-        cards.addEventListener("mouseenter", function () {
-            [3, 5, 7, 9].forEach(i => cards.childNodes[i].style.opacity = 1);
-            cards.childNodes[11].style.opacity = 0.8;
-            cards.childNodes[11].style.transform = "scale(0.9)";
+mm.add("(min-width: 800px)", () => {
+    function canAnimation() {
+        let tl = gsap.timeline({
+            scrollTrigger: {
+                trigger: ".page2",
+                start: "0% 90%",
+                end: "70% 50%",
+                scrub: true,
+            }
         })
-        cards.addEventListener("mouseleave", function () {
-            [3, 5, 7, 9].forEach(i => cards.childNodes[i].style.opacity = 0);
-            cards.childNodes[11].style.opacity = 1;
-            cards.childNodes[11].style.transform = "scale(1)";
+
+        tl.to("#can", {
+            top: "115%",
+            left: "15%"
+        }, "lime")
+
+        tl.to("#lime", {
+            width: "12%",
+            top: "160%",
+            left: "25%",
+            zIndex: 1
+        }, "lime")
+
+        tl.to("#leaf", {
+            top: "110%",
+            left: "75%",
+            zIndex: 1
+        }, "lime")
+
+        tl.to("#ice1", {
+            top: "120%",
+            left: "7%",
+            zIndex: 1
+        }, "lime")
+
+
+        let tl2 = gsap.timeline({
+            scrollTrigger: {
+                trigger: ".page3",
+                start: "0% 95%",
+                end: "25% 30%",
+                scrub: true,
+            }
         })
-    })
-}
+
+        tl2.to("#can", {
+            top: "212%",
+            left: "67.5%",
+            width: "15%",
+            display: "none"
+        })
 
 
-let cardcircle1 = document.querySelector(".cardcircle1");
-let cardcircle2 = document.querySelector(".cardcircle2");
-let cardcircle3 = document.querySelector(".cardcircle3");
-let cardcircle4 = document.querySelector(".cardcircle4");
+        gsap.to(".cardImage2", {
+            display: "block",
+            scrollTrigger: {
+                trigger: ".page3",
+                start: "0% 7%",
+                scrub: true,
+            }
+        })
+    }
 
-cardcircle1.addEventListener("click", function () {
-    window.location.href = "./Drinks/grapefruit.html";
-})
-cardcircle2.addEventListener("click", function () {
-    window.location.href = "./Drinks/lime.html";
-})
-cardcircle3.addEventListener("click", function () {
-    window.location.href = "./Drinks/peach.html";
-})
-cardcircle4.addEventListener("click", function () {
-    window.location.href = "./Drinks/blackcherry.html";
-})
+    let card = document.querySelectorAll(".card");
 
-
-
-
-
-
-
-function mouseFollower() {
-    card.forEach(function (details) {
-        details.addEventListener("mouseenter", function () {
-            gsap.to(details.childNodes[1], {
-                display: "block",
-                scale: 1
+    function cardAnimation() {
+        card.forEach(function (cards) {
+            cards.addEventListener("mouseenter", function () {
+                [3, 5, 7, 9].forEach(i => cards.childNodes[i].style.opacity = 1);
+                cards.childNodes[11].style.opacity = 0.8;
+                cards.childNodes[11].style.transform = "scale(0.9)";
+            })
+            cards.addEventListener("mouseleave", function () {
+                [3, 5, 7, 9].forEach(i => cards.childNodes[i].style.opacity = 0);
+                cards.childNodes[11].style.opacity = 1;
+                cards.childNodes[11].style.transform = "scale(1)";
             })
         })
+    }
 
-        details.addEventListener("mouseleave", function () {
-            gsap.to(details.childNodes[1], {
-                display: "none",
-                scale: 0
+
+    let cardcircle1 = document.querySelector(".cardcircle1");
+    let cardcircle2 = document.querySelector(".cardcircle2");
+    let cardcircle3 = document.querySelector(".cardcircle3");
+    let cardcircle4 = document.querySelector(".cardcircle4");
+
+    cardcircle1.addEventListener("click", function () {
+        window.location.href = "./Drinks/grapefruit.html";
+    })
+    cardcircle2.addEventListener("click", function () {
+        window.location.href = "./Drinks/lime.html";
+    })
+    cardcircle3.addEventListener("click", function () {
+        window.location.href = "./Drinks/peach.html";
+    })
+    cardcircle4.addEventListener("click", function () {
+        window.location.href = "./Drinks/blackcherry.html";
+    })
+
+
+
+
+
+
+
+    function mouseFollower() {
+        card.forEach(function (details) {
+            details.addEventListener("mouseenter", function () {
+                gsap.to(details.childNodes[1], {
+                    display: "block",
+                    scale: 1
+                })
+            })
+
+            details.addEventListener("mouseleave", function () {
+                gsap.to(details.childNodes[1], {
+                    display: "none",
+                    scale: 0
+                })
+            })
+
+            details.addEventListener("mousemove", function (data) {
+                gsap.to(details.childNodes[1], {
+                    x: data.x - details.getBoundingClientRect().x - 175,
+                    y: data.y - details.getBoundingClientRect().y - 285
+                })
             })
         })
+    }
 
-        details.addEventListener("mousemove", function (data) {
-            gsap.to(details.childNodes[1], {
-                x: data.x - details.getBoundingClientRect().x - 175,
-                y: data.y - details.getBoundingClientRect().y - 285
-            })
+    let buttons = document.querySelectorAll(".buttons");
+    buttons.forEach(function (click) {
+        click.addEventListener("click", function (event) {
+            event.stopPropagation();
+            window.location.href = "#page5";
         })
     })
-}
 
-let buttons = document.querySelectorAll(".buttons");
-buttons.forEach(function (click) {
-    click.addEventListener("click", function (event) {
-        event.stopPropagation();
-        window.location.href = "#page5";
+
+    canAnimation();
+    cardAnimation();
+    mouseFollower();
+});
+
+mm.add("(max-width: 799px)", () => {
+    function canAnimation() {
+        let tl = gsap.timeline({
+            scrollTrigger: {
+                trigger: ".page2",
+                start: "0% 90%",
+                end: "70% 50%",
+                scrub: true,
+            }
+        })
+
+        tl.to("#can", {
+            top: "130%",
+            left: "11%"
+        }, "lime")
+
+        tl.to("#lime", {
+            width: "25%",
+            top: "190%",
+            left: "45%",
+            zIndex: 1
+        }, "lime")
+
+        tl.to("#leaf", {
+            top: "100%",
+            left: "50%",
+            zIndex: 1
+        }, "lime")
+
+        tl.to("#ice1", {
+            top: "120%",
+            left: "7%",
+            zIndex: 1
+        }, "lime")
+
+
+        let tl2 = gsap.timeline({
+            scrollTrigger: {
+                trigger: ".page3",
+                start: "0% 95%",
+                end: "25% 57%",
+                scrub: true,
+            }
+        })
+
+        tl2.to("#can", {
+            top: "203.5%",
+            left: "32.5%",
+            width: "34.7%",
+            display: "none"
+        })
+
+
+        gsap.to(".cardImage2", {
+            display: "block",
+            scrollTrigger: {
+                trigger: ".page3",
+                start: "0% 32%",
+                scrub: true,
+            }
+        })
+    }
+
+
+    let cardcircle1 = document.querySelector(".cardcircle1");
+    let cardcircle2 = document.querySelector(".cardcircle2");
+    let cardcircle3 = document.querySelector(".cardcircle3");
+    let cardcircle4 = document.querySelector(".cardcircle4");
+
+    cardcircle1.addEventListener("click", function () {
+        window.location.href = "./Drinks/grapefruit.html";
     })
-})
+    cardcircle2.addEventListener("click", function () {
+        window.location.href = "./Drinks/lime.html";
+    })
+    cardcircle3.addEventListener("click", function () {
+        window.location.href = "./Drinks/peach.html";
+    })
+    cardcircle4.addEventListener("click", function () {
+        window.location.href = "./Drinks/blackcherry.html";
+    })
 
 
-
-
-
-
-canAnimation();
-cardAnimation();
-mouseFollower();
+    canAnimation();
+});
